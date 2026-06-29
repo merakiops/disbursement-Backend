7755acabf1371db2a36d9de73e1e5d5d986f4f65
Abhay Mishra <abhaymishraspn77@gmail.com>
2026-06-29 09:37:34 +0530

    allow for all

--- app/constants/constants.py
+++ app/constants/constants.py
@@ -1,18 +1,6 @@
 # constants.py
 
-ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://3.6.247.176:3000",
-    "http://disbursement.merakishippingservices.com:3000",
-    "http://localhost:8081",
-    "http://127.0.0.1:8081",
-    "http://0.0.0.0:8081",
-    "http://localhost:4200",
-    "http://127.0.0.1:4200",
-    "https://dev.merakishippingservices.com"
-
-]
+ALLOWED_ORIGINS = ["*"]
 
 ALLOWED_ROLE_IDS=[1,2]
 ACTIVE_STATUS='Y'
